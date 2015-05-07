55283dcaf335e8077eaab4cc8d74c22ce64de134
Liang Gao <lianggao@outlook.com>
2015-05-06 21:18:26 -0400

    Virtual Tour init

---
+++ tour/theSpheres.js
@@ -0,0 +1,204 @@
+var mySpheres =
+            
+            [
+                [
+                    [
+                      new Sphere(
+                        "IMG_7552.jpg",
+                        [new Arrow(-145,0,0,1),new Arrow(45,0,5,1)]
+                      ),
+                      new Sphere(
+                        "IMG_7553.jpg",
+                        [new Arrow(-145,0,0,2),new Arrow(-320,0,0,0)]
+                      ),
+                      new Sphere(
+                        "IMG_7554.jpg",
+                        [new Arrow(-145,0,0,3),new Arrow(-320,0,0,1)]
+                      ),
+                      new Sphere(
+                        "IMG_7555.jpg",
+                        [new Arrow(-145,0,0,4),new Arrow(-320,0,0,2)]
+                      ),
+                      new Sphere(
+                        "IMG_7556.jpg",
+                        [new Arrow(-145,0,0,5),new Arrow(-320,0,0,3)]
+                      ),
+                      new Sphere(
+                        "IMG_7558.jpg",
+                        [new Arrow(-145,0,0,6),new Arrow(-320,0,0,4)]
+                      ),
+                      new Sphere(
+                        "IMG_7562.jpg",
+                        [new Arrow(-145,0,0,7),new Arrow(-320,0,0,5)]
+                      ),
+                      new Sphere(
+                        "IMG_7563.jpg",
+                        [new Arrow(-145,0,0,8),new Arrow(-320,0,0,6)]
+                      ),
+                      new Sphere(
+                        "IMG_7564.jpg",
+                        [new Arrow(-145,0,0,9),new Arrow(-320,0,0,7)]
+                      ),
+                      new Sphere(
+                        "IMG_7569.jpg",
+                        [new Arrow(-120,0,1,0),new Arrow(-320,0,0,8)]
+                      )
+                    ],
+                    [
+						new Sphere(
+                        "IMG_8214.jpg",
+                        [new Arrow(-145,0,1,1),new Arrow(70,0,0,9),new Arrow(-55,0,1,5)]
+                        ),
+						
+						new Sphere(
+                        "IMG_8213.jpg",
+                        [new Arrow(-320,0,1,0),new Arrow(-55,0,1,2),new Arrow(-130,0,2,0)]
+                        ),
+						
+						new Sphere(
+                        "IMG_8212.jpg",
+                        [new Arrow(-40,0,1,3),new Arrow(125,0,1,1)]
+                        ),
+						
+						new Sphere(
+                        "IMG_8211.jpg",
+                        [new Arrow(-320,0,1,4),new Arrow(-235,0,1,2),new Arrow(-145,0,2,1)]
+                        ),
+						
+						new Sphere(
+                        "IMG_8210.jpg",
+                        [new Arrow(-145,0,1,3),new Arrow(125,0,1,5),new Arrow(30,0,3,0)]
+                        ),
+						
+						new Sphere(
+                        "IMG_8215.jpg",
+                        [new Arrow(-225,0,1,0),new Arrow(-55,0,1,4)]
+                        ),
+						
+                    ],
+					[
+						new Sphere(
+                        "IMG_8217.jpg",
+                        [new Arrow(50,0,1,1),new Arrow(-60,0,2,1),new Arrow(-210,1,2,1)]
+                        ),
+					
+						new Sphere(
+                        "IMG_8216.jpg",
+                        [new Arrow(-330,0,1,3),new Arrow(-235,0,2,0)]
+                        ),
+					
+					
+					],
+					
+					
+                    [
+                        new Sphere(
+                        "IMG_8577.jpg",
+                        [new Arrow(-160,0,1,4),new Arrow(-315,0,4,0)]
+                        ),
+                        
+                        
+                    ],
+                    [
+                       new Sphere(
+                        "IMG_8575.jpg",
+                        [new Arrow(-145,0,3,0),new Arrow(-235,0,4,1),new Arrow(-325,0,4,4)]
+                        ),
+						new Sphere(
+                        "IMG_8576.jpg",
+                        [new Arrow(-55,0,4,0),new Arrow(35,0,4,2)]
+                        ),
+						new Sphere(
+                        "IMG_8573.jpg",
+                        [new Arrow(-145,0,4,1),new Arrow(-55,0,4,3)]
+                        ),
+						new Sphere(
+                        "IMG_8572.jpg",
+                        [new Arrow(-55,0,4,4),new Arrow(-235,0,4,2),new Arrow(-315,0,5,0)]
+                        ),
+						new Sphere(
+                        "IMG_8574.jpg",
+                        [new Arrow(-235,0,4,3),new Arrow(215,0,4,0)]
+                        ),
+						
+                    ],
+					[
+						new Sphere(
+                        "IMG_8547.jpg",
+                        [new Arrow(230,0,4,3),new Arrow(115,0,5,1),new Arrow(-185,1,0,0)]
+                        ),
+						new Sphere(
+                        "IMG_8548.jpg",
+                        [new Arrow(-55,0,5,0),new Arrow(-140,0,0,0)]
+                        ),
+					]
+					
+                ],
+				[
+					[
+						
+						new Sphere(
+                        "IMG_8497.jpg",
+                        [new Arrow(-145,0,5,0),new Arrow(305,1,0,1)]
+                        ),
+						
+						new Sphere(
+                        "IMG_8496.jpg",
+                        [new Arrow(-245,1,0,0),new Arrow(185,1,1,0)]
+                        ),
+						
+					],
+					[
+						new Sphere(
+                        "IMG_8493.jpg",
+                        [new Arrow(-330,1,0,1),new Arrow(-145,1,1,1)]
+                        ),
+						new Sphere(
+                        "IMG_8494.jpg",
+                        [new Arrow(-320,1,1,0),new Arrow(-145,1,1,2)]
+                        ),
+						new Sphere(
+                        "IMG_8495.jpg",
+                        [new Arrow(-320,1,1,1),new Arrow(-170,1,2,0)]
+                        ),
+					],
+					[
+						new Sphere(
+                        "IMG_8611.jpg",
+                        [new Arrow(-55,1,3,0),new Arrow(-250,1,2,1),new Arrow(-335,1,1,2)]
+                        ),
+						new Sphere(
+                        "IMG_8612.jpg",
+                        [new Arrow(-30,1,2,0),new Arrow(-100,2,0,0),new Arrow(-170,0,2,0)]
+                        ),
+					],
+					[
+						new Sphere(
+                        "IMG_8476.jpg",
+                        [new Arrow(-250,1,2,0),new Arrow(-140,1,3,1)]
+                        ),
+						new Sphere(
+                        "IMG_8477.jpg",
+                        [new Arrow(-330,1,3,0)]
+                        ),
+					]
+				],
+				[
+					[
+						new Sphere(
+                        "IMG_8605.jpg",
+                        [new Arrow(-120,1,2,1),new Arrow(-330,2,0,1)]
+                        ),
+						new Sphere(
+                        "IMG_8604.jpg",
+                        [new Arrow(-145,2,0,0), new Arrow(-60,2,1,0)]
+                        ),
+					],
+					[
+						new Sphere(
+                        "IMG_8608.jpg",
+                        [new Arrow(-265,2,0,1)]
+                        ),
+					]
+				]
+            ];
