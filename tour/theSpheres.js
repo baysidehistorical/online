896c247c49531206cdbf8cc10a538acac291298e
Liang Gao <lianggao@outlook.com>
2015-06-11 20:01:54 -0400

    Changed file extension to uppercase JPG

--- tour/theSpheres.js
+++ tour/theSpheres.js
@@ -1,184 +1,184 @@
 var mySpheres =
-            
+
             [
                 [
                     [
                       new Sphere(
-                        "IMG_7552.jpg",
+                        "IMG_7552.JPG",
                         [new Arrow(-145,0,0,1),new Arrow(45,0,5,1)]
                       ),
                       new Sphere(
-                        "IMG_7553.jpg",
+                        "IMG_7553.JPG",
                         [new Arrow(-145,0,0,2),new Arrow(-320,0,0,0)]
                       ),
                       new Sphere(
-                        "IMG_7554.jpg",
+                        "IMG_7554.JPG",
                         [new Arrow(-145,0,0,3),new Arrow(-320,0,0,1)]
                       ),
                       new Sphere(
-                        "IMG_7555.jpg",
+                        "IMG_7555.JPG",
                         [new Arrow(-145,0,0,4),new Arrow(-320,0,0,2)]
                       ),
                       new Sphere(
-                        "IMG_7556.jpg",
+                        "IMG_7556.JPG",
                         [new Arrow(-145,0,0,5),new Arrow(-320,0,0,3)]
                       ),
                       new Sphere(
-                        "IMG_7558.jpg",
+                        "IMG_7558.JPG",
                         [new Arrow(-145,0,0,6),new Arrow(-320,0,0,4)]
                       ),
                       new Sphere(
-                        "IMG_7562.jpg",
+                        "IMG_7562.JPG",
                         [new Arrow(-145,0,0,7),new Arrow(-320,0,0,5)]
                       ),
                       new Sphere(
-                        "IMG_7563.jpg",
+                        "IMG_7563.JPG",
                         [new Arrow(-145,0,0,8),new Arrow(-320,0,0,6)]
                       ),
                       new Sphere(
-                        "IMG_7564.jpg",
+                        "IMG_7564.JPG",
                         [new Arrow(-145,0,0,9),new Arrow(-320,0,0,7)]
                       ),
                       new Sphere(
-                        "IMG_7569.jpg",
+                        "IMG_7569.JPG",
                         [new Arrow(-120,0,1,0),new Arrow(-320,0,0,8)]
                       )
                     ],
                     [
 						new Sphere(
-                        "IMG_8214.jpg",
+                        "IMG_8214.JPG",
                         [new Arrow(-145,0,1,1),new Arrow(70,0,0,9),new Arrow(-55,0,1,5)]
                         ),
-						
+
 						new Sphere(
-                        "IMG_8213.jpg",
+                        "IMG_8213.JPG",
                         [new Arrow(-320,0,1,0),new Arrow(-55,0,1,2),new Arrow(-130,0,2,0)]
                         ),
-						
+
 						new Sphere(
-                        "IMG_8212.jpg",
+                        "IMG_8212.JPG",
                         [new Arrow(-40,0,1,3),new Arrow(125,0,1,1)]
                         ),
-						
+
 						new Sphere(
-                        "IMG_8211.jpg",
+                        "IMG_8211.JPG",
                         [new Arrow(-320,0,1,4),new Arrow(-235,0,1,2),new Arrow(-145,0,2,1)]
                         ),
-						
+
 						new Sphere(
-                        "IMG_8210.jpg",
+                        "IMG_8210.JPG",
                         [new Arrow(-145,0,1,3),new Arrow(125,0,1,5),new Arrow(30,0,3,0)]
                         ),
-						
+
 						new Sphere(
-                        "IMG_8215.jpg",
+                        "IMG_8215.JPG",
                         [new Arrow(-225,0,1,0),new Arrow(-55,0,1,4)]
                         ),
-						
+
                     ],
 					[
 						new Sphere(
-                        "IMG_8217.jpg",
+                        "IMG_8217.JPG",
                         [new Arrow(50,0,1,1),new Arrow(-60,0,2,1),new Arrow(-210,1,2,1)]
                         ),
-					
+
 						new Sphere(
-                        "IMG_8216.jpg",
+                        "IMG_8216.JPG",
                         [new Arrow(-330,0,1,3),new Arrow(-235,0,2,0)]
                         ),
-					
-					
+
+
 					],
-					
-					
+
+
                     [
                         new Sphere(
-                        "IMG_8577.jpg",
+                        "IMG_8577.JPG",
                         [new Arrow(-160,0,1,4),new Arrow(-315,0,4,0)]
                         ),
-                        
-                        
+
+
                     ],
                     [
                        new Sphere(
-                        "IMG_8575.jpg",
+                        "IMG_8575.JPG",
                         [new Arrow(-145,0,3,0),new Arrow(-235,0,4,1),new Arrow(-325,0,4,4)]
                         ),
 						new Sphere(
-                        "IMG_8576.jpg",
+                        "IMG_8576.JPG",
                         [new Arrow(-55,0,4,0),new Arrow(35,0,4,2)]
                         ),
 						new Sphere(
-                        "IMG_8573.jpg",
+                        "IMG_8573.JPG",
                         [new Arrow(-145,0,4,1),new Arrow(-55,0,4,3)]
                         ),
 						new Sphere(
-                        "IMG_8572.jpg",
+                        "IMG_8572.JPG",
                         [new Arrow(-55,0,4,4),new Arrow(-235,0,4,2),new Arrow(-315,0,5,0)]
                         ),
 						new Sphere(
-                        "IMG_8574.jpg",
+                        "IMG_8574.JPG",
                         [new Arrow(-235,0,4,3),new Arrow(215,0,4,0)]
                         ),
-						
+
                     ],
 					[
 						new Sphere(
-                        "IMG_8547.jpg",
+                        "IMG_8547.JPG",
                         [new Arrow(230,0,4,3),new Arrow(115,0,5,1),new Arrow(-185,1,0,0)]
                         ),
 						new Sphere(
-                        "IMG_8548.jpg",
+                        "IMG_8548.JPG",
                         [new Arrow(-55,0,5,0),new Arrow(-140,0,0,0)]
                         ),
 					]
-					
+
                 ],
 				[
 					[
-						
+
 						new Sphere(
-                        "IMG_8497.jpg",
+                        "IMG_8497.JPG",
                         [new Arrow(-145,0,5,0),new Arrow(305,1,0,1)]
                         ),
-						
+
 						new Sphere(
-                        "IMG_8496.jpg",
+                        "IMG_8496.JPG",
                         [new Arrow(-245,1,0,0),new Arrow(185,1,1,0)]
                         ),
-						
+
 					],
 					[
 						new Sphere(
-                        "IMG_8493.jpg",
+                        "IMG_8493.JPG",
                         [new Arrow(-330,1,0,1),new Arrow(-145,1,1,1)]
                         ),
 						new Sphere(
-                        "IMG_8494.jpg",
+                        "IMG_8494.JPG",
                         [new Arrow(-320,1,1,0),new Arrow(-145,1,1,2)]
                         ),
 						new Sphere(
-                        "IMG_8495.jpg",
+                        "IMG_8495.JPG",
                         [new Arrow(-320,1,1,1),new Arrow(-170,1,2,0)]
                         ),
 					],
 					[
 						new Sphere(
-                        "IMG_8611.jpg",
+                        "IMG_8611.JPG",
                         [new Arrow(-55,1,3,0),new Arrow(-250,1,2,1),new Arrow(-335,1,1,2)]
                         ),
 						new Sphere(
-                        "IMG_8612.jpg",
+                        "IMG_8612.JPG",
                         [new Arrow(-30,1,2,0),new Arrow(-100,2,0,0),new Arrow(-170,0,2,0)]
                         ),
 					],
 					[
 						new Sphere(
-                        "IMG_8476.jpg",
+                        "IMG_8476.JPG",
                         [new Arrow(-250,1,2,0),new Arrow(-140,1,3,1)]
                         ),
 						new Sphere(
-                        "IMG_8477.jpg",
+                        "IMG_8477.JPG",
                         [new Arrow(-330,1,3,0)]
                         ),
 					]
@@ -186,17 +186,17 @@ var mySpheres =
 				[
 					[
 						new Sphere(
-                        "IMG_8605.jpg",
+                        "IMG_8605.JPG",
                         [new Arrow(-120,1,2,1),new Arrow(-330,2,0,1)]
                         ),
 						new Sphere(
-                        "IMG_8604.jpg",
+                        "IMG_8604.JPG",
                         [new Arrow(-145,2,0,0), new Arrow(-60,2,1,0)]
                         ),
 					],
 					[
 						new Sphere(
-                        "IMG_8608.jpg",
+                        "IMG_8608.JPG",
                         [new Arrow(-265,2,0,1)]
                         ),
 					]
