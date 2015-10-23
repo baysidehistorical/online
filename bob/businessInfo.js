68793d03b0572c60998076ef50f7f2f9919bd326
Raami Sharif <raamisharif@gmail.com>
2015-10-23 14:02:43 -0400

    fixed services picture

--- bob/businessInfo.js
+++ bob/businessInfo.js
@@ -51,7 +51,7 @@ var businessInfo =
 			new business("First Central Savings Bank","Denise Johnson/firstCentral.jpg","<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1510.9797812100744!2d-73.77110443318445!3d40.7629141213931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c261e07ac121ed%3A0x63f87a1d254244de!2sFirst+Central+Savings+Bank!5e0!3m2!1sen!2sus!4v1421438419564\" width=\"400\" height=\"300\" frameborder=\"0\" style=\"border:0\"></iframe>","First Central Savings Bank opened in 2005. <br/> Photo of branch manager, Maria Betzios<br/> Photo taken by Denise Johnson","42-12 Bell Boulevard, Bayside, NY 11361")
 		],
 		"Services",
-		"Services.JPG"
+		"services.jpg"
 	),
 		new businessType(
 		[
